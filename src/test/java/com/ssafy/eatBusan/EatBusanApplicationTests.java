af1b271c63e9988cdfb050e351e6ac7bc1ac3832
yunjae <coygj@gmail.com>
2026-05-15 14:37:32 +0900

    project init

---
+++ src/test/java/com/ssafy/eatBusan/EatBusanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.eatBusan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatBusanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
