8314a127447c039cf5bd77a6011fa4eec9c05c0b
sanghae <gobad820@gmail.com>
2026-05-22 12:37:35 +0900

    refactor: api 주소 변경

--- src/main/java/com/ssafy/eatBusan/postlike/controller/PostLikeController.java
+++ src/main/java/com/ssafy/eatBusan/postlike/controller/PostLikeController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/postlike")
+@RequestMapping("/api/posts")
 @RequiredArgsConstructor
 public class PostLikeController {
     private final PostLikeService postLikeService;
