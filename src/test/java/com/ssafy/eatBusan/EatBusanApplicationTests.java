3cba5f39ddda5d02ad2c374de746664c59815414
Kim Sanghae <pelikan@pusan.ac.kr>
2026-05-31 21:03:38 +0900

    chore: PR 범위 외 설정 및 테스트 제거

--- src/test/java/com/ssafy/eatBusan/EatBusanApplicationTests.java
+++ src/test/java/com/ssafy/eatBusan/EatBusanApplicationTests.java
@@ -3,10 +3,7 @@ package com.ssafy.eatBusan;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-		"spring.datasource.url=jdbc:mysql://localhost:3306/eatbusan_test?useSSL=false&serverTimezone=Asia/Seoul&characterEncoding=UTF-8&createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true",
-		"spring.jpa.hibernate.ddl-auto=create-drop"
-})
+@SpringBootTest
 class EatBusanApplicationTests {
 
 	@Test
