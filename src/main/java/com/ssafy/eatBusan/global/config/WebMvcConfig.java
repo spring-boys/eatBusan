6d4dbd981b6945aa1ba43cc3a1a26d7924d3a233
YUNJAEGOONER <coygyj@gmail.com>
2026-06-07 15:37:11 +0900

    feat: cors 설정을 위한 config 파일 작성

--- src/main/java/com/ssafy/eatBusan/global/config/WebMvcConfig.java
+++ src/main/java/com/ssafy/eatBusan/global/config/WebMvcConfig.java
@@ -3,8 +3,10 @@ package com.ssafy.eatBusan.global.config;
 import com.ssafy.eatBusan.auth.resolver.LoginMemberArgumentResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,8 +15,23 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
 
+    @Value("${spring.front.domain}")
+    private String frontDomain;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(frontDomain)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders("Authorization") // fe(js)에서 읽을 수 있는 헤더를 설정
+                .maxAge(3600); //preflight의 결과를 캐싱
+    }
+
 }
